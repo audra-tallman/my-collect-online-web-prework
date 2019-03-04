8a4a0980d525f6b31e0900c1c32dc85257bea75f
Audra Tallman <audra.tallman@hotmail.com>
2019-03-04 15:38:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+languages =  ["ruby", "javascript", "python", "objective-c"]
+students = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+
+def my_collect(array)
+   if array = []
+    counter = 0
+    my_collect(array) do |x|
+    counter += 1
+    end
+    my_collect(array)
+  end
+ if  my_collect(languages) do |language|
+    yield language.upcase
+  end
+  my_collect(languages)
+  end
+ if  my_collect(students) do |student|
+    yield student.split(" ").first
+  end
+  my_collect(students)
+  end
+end 
 
 
