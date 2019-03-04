c4dd33ed94dd7767395cb806f4a3da7d52201498
Audra Tallman <audra.tallman@hotmail.com>
2019-03-04 15:39:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,3 @@
-languages =  ["ruby", "javascript", "python", "objective-c"]
-students = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
 
 def my_collect(array)
    if array = []
@@ -8,17 +6,7 @@ def my_collect(array)
     counter += 1
     end
     my_collect(array)
-  end
- if  my_collect(languages) do |language|
-    yield language.upcase
-  end
-  my_collect(languages)
-  end
- if  my_collect(students) do |student|
-    yield student.split(" ").first
-  end
-  my_collect(students)
-  end
+ end
 end 
 
 
