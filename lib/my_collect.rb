fdcf031195768546cfa65ee987aee993986f0718
Audra Tallman <audra.tallman@hotmail.com>
2019-03-04 15:40:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
    i= 0
    collect = []
    while i < array.length
-    collect << yield(array[1])
+    collect << yield(array[i])
     counter += 1
     end
     my_collect(array)
